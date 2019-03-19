60cfd03c099e94e80fb795dbf841011b76f5887f
Pierre TACCHI <pyroh@icloud.com>
2019-03-19 16:32:07 +0100

    CGSize algebra cleanup. Keep only Int, Double & CGFloat

--- Sources/CoreGeometry/Extensions/CGSizeExtension.swift
+++ Sources/CoreGeometry/Extensions/CGSizeExtension.swift
@@ -44,50 +44,6 @@ extension CGSize {
     ///   - lhs: The size (a `CGSize` instance)
     ///   - rhs: The factor
     @inlinable
-    public static func * (lhs: CGSize, rhs: Int64) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func * (lhs: CGSize, rhs: Float) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func * (lhs: CGSize, rhs: Int8) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func * (lhs: CGSize, rhs: UInt8) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
     public static func * (lhs: CGSize, rhs: Double) -> CGSize {
         let factor = CGFloat(rhs)
         return CGSize(width: lhs.width * factor, height: lhs.height * factor)
@@ -99,84 +55,7 @@ extension CGSize {
     ///   - lhs: The size (a `CGSize` instance)
     ///   - rhs: The factor
     @inlinable
-    public static func * (lhs: CGSize, rhs: Int16) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func * (lhs: CGSize, rhs: UInt32) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func * (lhs: CGSize, rhs: UInt) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func * (lhs: CGSize, rhs: UInt16) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func * (lhs: CGSize, rhs: Int32) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func * (lhs: CGSize, rhs: UInt64) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
     public static func * (lhs: CGSize, rhs: CGFloat) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width * factor, height: lhs.height * factor)
-    }
-    
-    /// Multiplies both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func * (lhs: CGSize, rhs: Float80) -> CGSize {
         let factor = CGFloat(rhs)
         return CGSize(width: lhs.width * factor, height: lhs.height * factor)
     }
@@ -198,50 +77,6 @@ extension CGSize {
     ///   - lhs: The size (a `CGSize` instance)
     ///   - rhs: The factor
     @inlinable
-    public static func / (lhs: CGSize, rhs: Int64) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func / (lhs: CGSize, rhs: Float) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func / (lhs: CGSize, rhs: Int8) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func / (lhs: CGSize, rhs: UInt8) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
     public static func / (lhs: CGSize, rhs: Double) -> CGSize {
         let factor = CGFloat(rhs)
         return CGSize(width: lhs.width / factor, height: lhs.height / factor)
@@ -253,84 +88,7 @@ extension CGSize {
     ///   - lhs: The size (a `CGSize` instance)
     ///   - rhs: The factor
     @inlinable
-    public static func / (lhs: CGSize, rhs: Int16) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func / (lhs: CGSize, rhs: UInt32) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func / (lhs: CGSize, rhs: UInt) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func / (lhs: CGSize, rhs: UInt16) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func / (lhs: CGSize, rhs: Int32) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func / (lhs: CGSize, rhs: UInt64) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
     public static func / (lhs: CGSize, rhs: CGFloat) -> CGSize {
-        let factor = CGFloat(rhs)
-        return CGSize(width: lhs.width / factor, height: lhs.height / factor)
-    }
-    
-    /// Devides both width and height values of a size by a given factor.
-    ///
-    /// - Parameters:
-    ///   - lhs: The size (a `CGSize` instance)
-    ///   - rhs: The factor
-    @inlinable
-    public static func / (lhs: CGSize, rhs: Float80) -> CGSize {
         let factor = CGFloat(rhs)
         return CGSize(width: lhs.width / factor, height: lhs.height / factor)
     }
