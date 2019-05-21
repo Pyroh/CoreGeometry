7d211d326c1b81bcca6cf13ab1f2ebc5613b619e
Pierre TACCHI <pyroh@icloud.com>
2019-05-21 16:54:11 +0200

    [ADD] CGPoint algebrae

--- Sources/CoreGeometry/Extensions/CGPointExtension.swift
+++ Sources/CoreGeometry/Extensions/CGPointExtension.swift
@@ -86,3 +86,75 @@ public extension CGPoint {
         self = self.rotated(relativeTo: center, by: angle)
     }
 }
+
+public extension CGPoint {
+    @inlinable
+    static func *(lhs: CGPoint, rhs: Int) -> CGPoint {
+        return lhs * CGFloat(rhs)
+    }
+    
+    @inlinable
+    static func *(lhs: Int, rhs: CGPoint) -> CGPoint {
+        return rhs * lhs
+    }
+    
+    @inlinable
+    static func *(lhs: CGPoint, rhs: Double) -> CGPoint {
+        return lhs * CGFloat(rhs)
+    }
+    
+    @inlinable
+    static func *(lhs: Double, rhs: CGPoint) -> CGPoint {
+        return rhs * lhs
+    }
+    
+    @inlinable
+    static func *(lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+        return CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+    
+    @inlinable
+    static func *(lhs: CGFloat, rhs: CGPoint) -> CGPoint {
+        return rhs * lhs
+    }
+    
+    @inlinable
+    static func /(lhs: CGPoint, rhs: Int) -> CGPoint {
+        return lhs / CGFloat(rhs)
+    }
+    
+    @inlinable
+    static func /(lhs: Int, rhs: CGPoint) -> CGPoint {
+        return rhs / lhs
+    }
+    
+    @inlinable
+    static func /(lhs: CGPoint, rhs: Double) -> CGPoint {
+        return lhs / CGFloat(rhs)
+    }
+    
+    @inlinable
+    static func /(lhs: Double, rhs: CGPoint) -> CGPoint {
+        return rhs / lhs
+    }
+    
+    @inlinable
+    static func /(lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+        return CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
+    }
+    
+    @inlinable
+    static func /(lhs: CGFloat, rhs: CGPoint) -> CGPoint {
+        return rhs / lhs
+    }
+    
+    @inlinable
+    static func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    
+    @inlinable
+    static func -(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+}
